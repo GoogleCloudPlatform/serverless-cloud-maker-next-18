78874734adddc0d2ea7fab5e8f6c688a31f1b003
Jake Seaton <jseaton@google.com>
2018-07-02 15:51:28 -0700

    adds import of vision function

--- functions/index.js
+++ functions/index.js
@@ -21,6 +21,7 @@ const transformApplyRotate = require('./rotate')
 const transformApplyReflect = require('./reflect')
 const transformApplyFilter = require('./filter')
 const transformApplyBorder = require('./border')
+const transformApplyCaption = require('./caption')
 
 module.exports = {
 	// basic functions
@@ -32,4 +33,7 @@ module.exports = {
     transformApplyReflect,
     transformApplyFilter,
     transformApplyBorder,
+    // vision transformations
+    transformApplyCaption,
+
 }
